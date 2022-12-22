e24710e5536731a5cda393f7883638c0ae8193f2
Robeliom15 <robeliom15@gmail.com>
2022-12-21 20:35:24 -0800

    Finish Cleaning and create tables for SQL

---
+++ queries/tables.sql
@@ -0,0 +1,24 @@
+-- Create Main CSV file table
+CREATE TABLE anime_clean (
+ID VARCHAR,
+Title VARCHAR,
+English VARCHAR,
+Type  VARCHAR,
+Episodes VARCHAR,
+Status VARCHAR,
+Start_Aired VARCHAR,
+End_Aired VARCHAR,
+Premiered VARCHAR,
+Licensors VARCHAR,
+Studios VARCHAR,
+Source VARCHAR,
+Genres VARCHAR,
+Demographics VARCHAR,
+Duration_Minutes VARCHAR,
+Rating VARCHAR,
+Score VARCHAR,
+Popularity VARCHAR,
+Favorites VARCHAR
+);
+
+--
